b288b950e38828cfd747ad5b7f24d24852243b11
GaganaSgowda <jsgagana39@gmail.com>
2023-04-03 16:17:03 +0530

    first commit

---
+++ src/Screenshot.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Screenshot {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\eclipse-workspace\\DEMO\\server1\\chromedriver.exe" );
+		WebDriver driver=new ChromeDriver();
+	}
+
+}
---
+++ src/SeleniumConcepts/Screenshot.java
@@ -0,0 +1,11 @@
+package SeleniumConcepts;
+
+public class Screenshot {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\eclipse-workspace\\DEMO\\server1\\chromedriver.exe" );
+		WebDriver driver=new ChromeDriver();
+
+	}
+
+}
